bac8f1c85c5fad4bfc8860132ac88ac0918ab53b
Roy <roy@yucing.com.tw>
2023-11-11 16:50:29 +0800

    update trail

--- trail.js
+++ trail.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const numberOfElements = 20; // 軌跡元素的數量
+    const numberOfElements = 50; // 增加軌跡元素的數量
     const elements = [];
+    const delay = 2; // 增加更新延遲
+    let updateCounter = 0;
 
     for (let i = 0; i < numberOfElements; i++) {
         let el = document.createElement('img');
-        el.src = './light.png'; // 指定圖片路徑
+        el.src = './light.png';
         el.className = 'trail';
         document.body.appendChild(el);
         elements.push(el);
@@ -20,27 +22,18 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     document.addEventListener('mousemove', (e) => {
-        updatePosition(e.pageX, e.pageY);
+        updateCounter++;
+        if (updateCounter % delay === 0) {
+            updatePosition(e.pageX, e.pageY);
+        }
     });
 
     document.addEventListener('touchmove', (e) => {
-        e.preventDefault(); // 防止窗口拖動
-        var touch = e.touches[0];
-        updatePosition(touch.pageX, touch.pageY);
-    });
-
-    // 防止觸摸時的縮放和滾動
-    document.addEventListener('touchstart', (e) => {
-        if (e.touches.length > 1) {
-            e.preventDefault();
+        e.preventDefault();
+        updateCounter++;
+        if (updateCounter % delay === 0) {
+            var touch = e.touches[0];
+            updatePosition(touch.pageX, touch.pageY);
         }
-    }, { passive: false });
-
-    document.addEventListener('touchend', (e) => {
-        e.preventDefault();
-    }, { passive: false });
-
-    document.addEventListener('touchcancel', (e) => {
-        e.preventDefault();
-    }, { passive: false });
+    });
 });
